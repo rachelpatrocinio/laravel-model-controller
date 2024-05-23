775ea0a66f65a1a3352c9049a345f42990bfebd1
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-05-23 14:24:41 +0200

    Added: controller MovieController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/', [MovieController::class, 'home'])->name('home');
