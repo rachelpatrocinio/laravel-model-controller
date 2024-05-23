7d59a4f7a45a9c84f412e9b5424adb7e622c2041
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-05-23 14:38:29 +0200

    Added: model Movie

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
@@ -9,6 +10,7 @@ class MovieController extends Controller
     //
     public function home()
     {
-        return view('home');
+        $movies = Movie::all();
+        return view('home', compact('movies'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,9 @@
 
 @section('content')
 <h1>Home Page</h1>
+<ul>
+    @foreach($movies as $movie)
+    <li>{{ $movie->title }}</li>
+    @endforeach
+</ul>
 @endsection
